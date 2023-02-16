f5c8f80edc67e17f66e10359acb028d6fcddd20c
Nathan <lingsnathan96@gmail.com>
2023-02-16 02:25:48 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program prints a line using puts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
