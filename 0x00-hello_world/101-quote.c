271b5ff75271148a4de04e2a84e6e2841ecc924a
Nathan <lingsnathan96@gmail.com>
2023-02-19 04:57:55 -0800

    UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
 /**
  * main - A c program that prints a line to a standard error
  * Return 1 (Success)
- * */
+ * /
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r",stdout);
-	return (1)
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	return (1);
 }
