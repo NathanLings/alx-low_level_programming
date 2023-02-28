f8efef1db5facb044e6ffe80e56633db5e4720a2
Nathan <lingsnathan96@gmail.com>
2023-02-28 01:57:20 -0800

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,7 +13,7 @@ int _atoi(char *s)
 	i = 0;
 	d = 0;
 	n = 0;
-	len = 0
+	len = 0;
 	f = 0;
 	digit = 0;
 
@@ -30,7 +30,7 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n = n * 10 + digit
+			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
