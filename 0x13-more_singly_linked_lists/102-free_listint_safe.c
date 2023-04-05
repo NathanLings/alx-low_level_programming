73cb1401e41bdfd99d94bb4d6a6c11ef2e74920f
Nathan <lingsnathan96@gmail.com>
2023-04-05 11:46:05 -0700

    All Tasks

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,10 +11,10 @@ size_t free_listint_safe(listint_t **h)
 	size_t len = 0;
 	int diff;
 	listint_t *temp;
-	
+
 	if (!h || !*h)
 		return (0);
-	
+
 	while (*h)
 	{
 		diff = *h - (*h)->next;
@@ -33,8 +33,8 @@ size_t free_listint_safe(listint_t **h)
 			break;
 		}
 	}
-	
+
 	*h = NULL;
-	
+
 	return (len);
 }
